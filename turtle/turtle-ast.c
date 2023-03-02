195b8e0f889cde649c4ff28644f53ae59af92f2d
pppg <paolopereiragehant@gmail.com>
2023-03-02 12:07:54 +0100

    base

---
+++ turtle/turtle-ast.c
@@ -0,0 +1,75 @@
+#include "turtle-ast.h"
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define PI 3.141592653589793
+
+struct ast_node *make_expr_value(double value) {
+	struct ast_node *node = calloc(1, sizeof(struct ast_node));
+	node->kind = KIND_EXPR_VALUE;
+	node->u.value = value;
+	return node;
+}
+
+
+struct ast_node *make_cmd_forward(struct ast_node *expr) {
+  struct ast_node *node = calloc(1, sizeof(struct ast_node));
+  node->kind = KIND_CMD_SIMPLE;
+  node->u.cmd = CMD_FORWARD;
+  node->children_count = 1;
+  node->children[0] = expr;
+  return node;
+}
+
+struct ast_node *make_cmd_color(struct ast_node *exprRed, struct ast_node *exprGreen, struct ast_node *exprBlue) {
+  struct ast_node *node = calloc(1, sizeof(struct ast_node));
+  node->kind = KIND_CMD_SIMPLE;
+  node->u.cmd = CMD_COLOR;
+  node->children_count = 3;
+  node->children[0] = expr;
+  node->children[1] = expr;
+  node->children[2] = expr;
+  return node;
+}
+
+struct ast_node *make_cmd_right(struct ast_node *expr) {
+  struct ast_node *node = calloc(1, sizeof(struct ast_node));
+  node->kind = KIND_CMD_SIMPLE;
+  node->u.cmd = CMD_RIGHT;
+  node->children_count = 1;
+  node->children[0] = expr;
+  return node;t
+}
+
+void ast_destroy(struct ast *self) {
+
+}
+
+/*
+ * context
+ */
+
+void context_create(struct context *self) {
+
+}
+
+/*
+ * eval
+ */
+
+void ast_eval(const struct ast *self, struct context *ctx) {
+
+}
+
+/*
+ * print
+ */
+
+void ast_print(const struct ast *self) {
+
+}
